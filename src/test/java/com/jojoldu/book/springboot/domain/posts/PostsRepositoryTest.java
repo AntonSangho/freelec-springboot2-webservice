2d55e4e644a662cfcb3e67e7d7577554ce16004c
anton <sanghoemail@gmail.com>
2025-09-05 16:28:48 +0900

    날짜 변경

--- src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -47,7 +47,7 @@ public class PostsRepositoryTest {
     @Test
     public void BaseTimeEntity_등록() {
         //given
-        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        LocalDateTime now = LocalDateTime.of(2025, 9, 5, 2, 38, 0);
         postsRepository.save(Posts.builder()
                 .title("title")
                 .content("content")
