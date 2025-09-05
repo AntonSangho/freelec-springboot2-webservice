86d3e30fab6b0da0ab2ca9cc8479fe30c0134ac0
anton <sanghoemail@gmail.com>
2025-09-05 17:28:15 +0900

    삭제 이벤트 index.js 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -46,7 +46,25 @@ var main = {
                      }).fail(function (error) {
                          alert(JSON.stringify(error));
                      });
-                     }
+                     },
+    delete : function () {
+                    var id = $('#id').val();
+                 };
+
+                 var id = $('#id').val();
+                 $.ajax({
+                             type: 'DELET',
+                             url: '/api/v1/posts/'+id,
+                             dataType: 'json',
+                             contentType:'application/json; charset=utf-8',
+                             data: JSON.stringify(data)
+                         }).done(function() {
+                             alert('글이 삭제되었습니다.');
+                             window.location.href = '/';
+                         }).fail(function (error) {
+                             alert(JSON.stringify(error));
+                         });
+                 }
 };
 
 main.init();
