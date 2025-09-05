f472c11d79f0dbe7046d9f608deeb8ca30c4faa6
anton <sanghoemail@gmail.com>
2025-09-05 17:32:02 +0900

    삭제 기능 추가

--- src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
+++ src/main/java/com/jojoldu/book/springboot/web/PostsApiController.java
@@ -26,6 +26,12 @@ public class PostsApiController {
     public  Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
+    // 삭제하기
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
 
     //조회하기
     @GetMapping("/api/v1/posts/{id}")
