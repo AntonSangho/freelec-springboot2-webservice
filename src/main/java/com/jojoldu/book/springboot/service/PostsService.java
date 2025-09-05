9583b63780ed9f4a174c167cbe50932d8909ee4e
anton <sanghoemail@gmail.com>
2025-09-05 16:37:41 +0900

    PostService에 조회기능 추가

--- src/main/java/com/jojoldu/book/springboot/service/PostsService.java
+++ src/main/java/com/jojoldu/book/springboot/service/PostsService.java
@@ -1,6 +1,7 @@
 package com.jojoldu.book.springboot.service;
 
 import com.jojoldu.book.springboot.domain.posts.PostsRepository;
+import com.jojoldu.book.springboot.web.dto.PostsListResponseDto;
 import com.jojoldu.book.springboot.web.dto.PostsSaveRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsUpdateRequestDto;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
@@ -8,6 +9,9 @@ import com.jojoldu.book.springboot.domain.posts.Posts;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,6 +33,12 @@ public class PostsService {
         return id;
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
