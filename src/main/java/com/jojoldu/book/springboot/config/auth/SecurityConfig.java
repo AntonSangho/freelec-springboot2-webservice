e7bf09f169ca628bdfa13f1388feb93519d99dbf
AntonSangho <sanghoemail@gmail.com>
2025-09-06 21:25:46 +0900

    시큐리티 설정

---
+++ src/main/java/com/jojoldu/book/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.jojoldu.book.springboot.config.auth;
+
+import com.jojoldu.book.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+// Spring Security 설정들을 활성화시켜줍니다.
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable() //h2-console 화면을 사용하기 위해 해당 옵션들을 disable합니다
+                .and()
+                .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점입니다.
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
+                .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // 권한 관리 대상을 지정하는 옵션입니다.
+                .anyRequest().authenticated() // 설정된 값들 이외 나머지 URL들을 나타냅니다.
+                .and()
+                .logout()
+                .logoutSuccessUrl("/") // 로그아웃 기능에 대한 여러 설정의 진입점입니다.
+                .and()
+                .oauth2Login() // OAuth 2 로그인 기능에 대한 여러 설정의 진입점입니다.
+                .userInfoEndpoint() // OAuth 2 로그인 성공 이후 사용자 정보를 가져올 떄의 설정들을 담당합니다.
+                .userService(customOAuth2UserService); // 소설 로그인 성공시 후속 조치를 진행할 UserService 인터페이스의 구현체를 등록합니다.
+    }
+}
