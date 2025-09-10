80069fb53fa76b582ba1853c6f72306a29bb75ea
AntonSangho <sanghoemail@gmail.com>
2025-09-10 20:27:30 +0900

    충돌하면서 오류수정

--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -5,11 +5,7 @@ import com.jojoldu.book.springboot.config.auth.dto.SessionUser;
 import com.jojoldu.book.springboot.service.PostsService;
 import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import jakarta.servlet.http.HttpSession;
-
-import com.jojoldu.book.springboot.service.PostsService;
-import com.jojoldu.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.h2.engine.Mode;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,35 +14,30 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
-
+    
     private final PostsService postsService;
     private final HttpSession httpSession;
-
+    
     @GetMapping("/")
-    public  String index(Model model, @LoginUser SessionUser user) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
-        if (user !=null) {
+        
+        if (user != null) {
             model.addAttribute("userName", user.getName());
         }
-
-
-    @GetMapping("/")
-    public  String index(Model model) {
-        model.addAttribute("posts", postsService.findAllDesc());
-        return "index" ;
+        
+        return "index";
     }
-
+    
     @GetMapping("/posts/save")
-    public String postSave() {
+    public String postsSave() {
         return "posts-save";
     }
-
+    
     @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id, Model model){
+    public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
-
         return "posts-update";
     }
 }
